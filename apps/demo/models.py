01a4dbc51926269bc1d741ccd6ca912217d81d9d
Arpit <arpit@quandlelabs.com>
2024-12-08 11:59:31 +0530

    comit model

--- apps/demo/models.py
+++ apps/demo/models.py
@@ -18,14 +18,14 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class User(models.Model):
-    id = models.AutoField(primary_key=True)  # Auto-incrementing ID field
-    password = models.CharField(max_length=128)  # Password field
-    last_login = models.DateTimeField(null=True, blank=True)  # Last login datetime
-    is_superuser = models.BooleanField(default=False)  # Boolean for superuser status
-    username = models.CharField(max_length=150, unique=True)  # Username (unique)
-    last_name = models.CharField(max_length=150)  # Last name
-    email = models.EmailField(max_length=254)  # Email field
-    is_staff = models.BooleanField(default=False)  # Is staff flag
-    is_active = models.BooleanField(default=True)  # Is active flag
-    date_joined = models.DateTimeField(auto_now_add=True)  # Date the user joined
-    first_name = models.CharField(max_length=150)  # First name
+    id = models.AutoField(primary_key=True)  
+    password = models.CharField(max_length=128)  
+    last_login = models.DateTimeField(null=True, blank=True)  
+    is_superuser = models.BooleanField(default=False)  
+    username = models.CharField(max_length=150, unique=True) 
+    last_name = models.CharField(max_length=150) 
+    email = models.EmailField(max_length=254)  
+    is_staff = models.BooleanField(default=False)  
+    is_active = models.BooleanField(default=True) 
+    date_joined = models.DateTimeField(auto_now_add=True)  
+    first_name = models.CharField(max_length=150)  
